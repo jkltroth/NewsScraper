1d09692b84c2fcf2968d511987e7bf44d5c16f11
James Roth <jkltroth@gmail.com>
2018-02-09 21:12:06 -0500

    Removed ScrapedArticle model now that we are no longer saving scraped articles to database

--- models/index.js
+++ models/index.js
@@ -1,7 +1,6 @@
 // Exporting an object containing all of our models
 
 module.exports = {
-    ScrapedArticle: require("./ScrapedArticle"),
     SavedArticle: require("./SavedArticle"),
     Note: require("./Note")
   };
