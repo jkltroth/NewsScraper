2d6cd8b6a9ace20215c91dc0063d8a463184f3d5
James Roth <jkltroth@gmail.com>
2018-02-09 21:08:26 -0500

    Srape successfully rendering data on DOM

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,14 +1,14 @@
 
-// When you click the savenote button
+// When you click the scrape button
 $(document).on("click", "#scrapeBtn", function() {
 
         // Now make an ajax call for to scrape the news site
         $.ajax({
                 method: "GET",
                 url: "/scrape"
-            }) // With that done, add the note information to the page
+            }) // With that done, reload the page
             .done(function (data) {
-                // console.log(data);
+        
                 location.reload();
                 
             });
